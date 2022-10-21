d0447b7ec45eeb811d08f74de0df004b871c8e46
Demilade somide <demisomide@gmail.com>
2022-10-21 20:12:30 +0100

    added week-2 practice files

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!(); // print an empty line
+
+    println!("hello "); // prints hello
+
+    println!("format {} arguments", "some"); // prints format some arguments
+
+    println!("My ame is {}. I am the class rep of stream 2", "Demilade Somide");
+
+}
